23fded6a412ce9121b01b81b10b0371626ab9cdc
JISUNG PARK <meowbutlerdev@gmail.com>
2025-03-10 14:46:36 +0900

    docs: 기본 데이터 타입

---
+++ week1/5-DataType.playground/Contents.swift
@@ -0,0 +1,191 @@
+import UIKit
+
+// Swift Style Guide No.3 적용하기
+// let age: Int 처럼 : 앞에는 공백이 없게 뒤에는 공백을 넣음
+
+// 변수와 상수를 사용하여 다양한 기본 데이터 타입 선언
+// Int, Float, Double, String, Bool 타입 선언하고 출력
+// 각각의 타입 마다 10개 이상 만들기
+var age = 25
+print(age)
+
+var year = 2025
+print(year)
+
+var month = 3
+print(month)
+
+var day = 12
+print(day)
+
+var score = 99
+print(score)
+
+var totalSteps = 14500
+print(totalSteps)
+
+var rank = 1
+print(rank)
+
+var temperatureInCelsius = -5
+print(temperatureInCelsius)
+
+var pagesInBook = 350
+print(pagesInBook)
+
+var maxUsers = 50000
+print(maxUsers)
+
+var price = 19.99
+print(price)
+
+var discountRate = 0.15
+print(discountRate)
+
+var piValue = 3.141592
+print(piValue)
+
+var taxRate = 0.07
+print(taxRate)
+
+var latitude = 37.5665
+print(latitude)
+
+var longitude = 126.9780
+print(longitude)
+
+var itemWeight = 2.5
+print(itemWeight)
+
+var speedLimit = 55.75
+print(speedLimit)
+
+var exchangeRate = 1342.45
+print(exchangeRate)
+
+var testScore = 87.6
+print(testScore)
+
+var precisePi = 3.141592653589793
+print(precisePi)
+
+var goldenRatio = 1.618033988749895
+print(goldenRatio)
+
+var lightSpeed = 299792458.0
+print(lightSpeed)
+
+var eulersNumber = 2.718281828459045
+print(eulersNumber)
+
+var plankConstant = 6.62607015e-34
+print(plankConstant)
+
+var gravityAcceleration = 9.80665
+print(gravityAcceleration)
+
+var waterBoilingPoint = 100.0
+print(waterBoilingPoint)
+
+var distanceToMoon = 384400.0
+print(distanceToMoon)
+
+var humanBodyTemperature = 36.61
+print(humanBodyTemperature)
+
+var maxStorageCapacity = 256.75
+print(maxStorageCapacity)
+
+var userName = "춘장"
+print(userName)
+
+var country = "한국"
+print(country)
+
+var favoriteMovie = "아이언맨"
+print(favoriteMovie)
+
+var favoriteFood = "북어"
+print(favoriteFood)
+
+var welcomeMessage = "야옹!"
+print(welcomeMessage)
+
+var bookTitle = "감태 냄새맡기"
+print(bookTitle)
+
+var email = "chunjang@meju.gamtae"
+print(email)
+
+var phoneNumber = "+82-10-1234-5678"
+print(phoneNumber)
+
+var address = "서울시 은평구"
+print(address)
+
+var motivationalQuote = "야옹! 야옹!"
+print(motivationalQuote)
+
+var isLoggedIn = true
+print(isLoggedIn)
+
+var hasPremiumAccess = false
+print(hasPremiumAccess)
+
+var isDarkModeEnabled = true
+print(isDarkModeEnabled)
+
+var isWeekend = false
+print(isWeekend)
+
+var isAbove18 = true
+print(isAbove18)
+
+var hasCompletedTask = false
+print(hasCompletedTask)
+
+var isAvailable = true
+print(isAvailable)
+
+var hasDiscount = false
+print(hasDiscount)
+
+var isVerified = true
+print(isVerified)
+
+var isBatteryLow = false
+print(isBatteryLow)
+
+// 튜플 선언하고 인덱스 값 사용하여 출력
+// 2개의 값으로 묶인 튜플 5개 생성한 뒤 출력
+var cat1 = ("춘장", 4)
+print(cat1.0, cat1.1)
+
+var cat2 = ("메주", 2)
+print(cat2.0, cat2.1)
+
+var cat3 = ("감태", 28)
+print(cat3.0, cat3.1)
+
+var person1 = ("집사1", 20)
+print(person1.0, person1.1)
+
+var person2 = ("집사2", 22)
+print(person2.0, person2.1)
+
+// 3개의 값으로 묶인 튜플 5개 생성한 뒤 출력
+// 3개의 값을 가진 튜플 선언 (이름 지정)
+var meow1 = (name: "춘장", age: 4, grade: "A")
+print(meow1.name, meow1.age, meow1.grade)
+
+var meow2 = (name: "메주", age: 2, grade: "B")
+print(meow2.name, meow2.age, meow2.grade)
+
+var meow3 = (name: "감태", age: 2, grade: "C")
+print(meow3.name, meow3.age, meow3.grade)
+
+var butler1 = (name: "집사1", age: 35, grade: "B+")
+print(butler1.name, butler1.age, butler1.grade)
+
+var butler2 = (name: "집사2", age: 22, grade: "A+")
+print(butler2.name, butler2.age, butler2.grade)
