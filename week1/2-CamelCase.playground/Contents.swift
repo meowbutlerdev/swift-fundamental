b3cf34f0269eb6ffdb2fab6b1e4ffca12d46e89a
JISUNG PARK <meowbutlerdev@gmail.com>
2025-03-10 14:46:00 +0900

    docs: 카멜 케이스

---
+++ week1/2-CamelCase.playground/Contents.swift
@@ -0,0 +1,54 @@
+import UIKit
+
+/**
+ my name is brody // 기본 문장
+ myNameIsBrody
+
+ Hello World // 기본 문장
+ HelloWorld
+
+ my car is fast
+ myCarIsFast // 이렇게 수정해주세요.
+
+ she loves coding
+ sheLovesCoding
+
+ we are learning swift
+ weAreLearningSwift
+
+ the sky is blue
+ theSkyIsBlue
+
+ i like coffee
+ iLikeCoffee
+
+ my favorite color is red
+ myFavoriteColorIsRed
+
+ this is a test
+ thisIsATest
+
+ programming is fun
+ programmingIsFun
+
+ apple makes great products
+ appleMakesGreatProducts
+
+ teamwork is important
+ teamworkIsImportant
+
+ the cat is sleeping
+ theCatIsSleeping
+
+ i want to travel
+ iWantToTravel
+
+ we are developers
+ weAreDevelopers
+
+ learning swift is easy
+ learningSwiftIsEasy
+
+ my computer is new
+ myComputerIsNew
+ */
