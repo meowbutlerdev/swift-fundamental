79bdfa946d40456307461f6b35021b00626e866f
JISUNG PARK <meowbutlerdev@gmail.com>
2025-03-10 14:46:16 +0900

    docs: 변수와 상수

---
+++ week1/3-Variable-Constant.playground/Contents.swift
@@ -0,0 +1,413 @@
+import UIKit
+
+// 변수 상수 실습
+
+
+// 기본 선언 방법
+// 변수와 상수를 각각 20개씩 만들어보세요.
+var userName: String
+userName = "춘장"
+print(userName)
+
+var userAge: Int
+userAge = 28
+print(userAge)
+
+var userHeight: Double
+userHeight = 172.5
+print(userHeight)
+
+var isUserActive: Bool
+isUserActive = true
+print(isUserActive)
+
+var accountBalance: Float
+accountBalance = 3050.75
+print(accountBalance)
+
+var country: String
+country = "한국"
+print(country)
+
+var favoriteColor: String
+favoriteColor = "검정"
+print(favoriteColor)
+
+var dailySteps: Int
+dailySteps = 9500
+print(dailySteps)
+
+var temperature: Double
+temperature = 36.8
+print(temperature)
+
+var isRaining: Bool
+isRaining = false
+print(isRaining)
+
+var score: Int
+score = 99
+print(score)
+
+var ranking: Int
+ranking = 3
+print(ranking)
+
+var isPremiumUser: Bool
+isPremiumUser = true
+print(isPremiumUser)
+
+var discountRate: Double
+discountRate = 0.2
+print(discountRate)
+
+var stockPrice: Float
+stockPrice = 650.45
+print(stockPrice)
+
+var message: String
+message = "Hello, World!"
+print(message)
+
+var numberOfMessages: Int
+numberOfMessages = 15
+print(numberOfMessages)
+
+var lastLoginDate: String
+lastLoginDate = "2025-03-11"
+print(lastLoginDate)
+
+var appTheme: String
+appTheme = "Dark"
+print(appTheme)
+
+var batteryLevel: Int
+batteryLevel = 85
+print(batteryLevel)
+
+let pi: Double
+pi = 3.141592
+print(pi)
+
+let appName: String
+appName = "메주 키우기"
+print(appName)
+
+let maxUsers: Int
+maxUsers = 1000000
+print(maxUsers)
+
+let minPasswordLength: Int
+minPasswordLength = 8
+print(minPasswordLength)
+
+let earthGravity: Double
+earthGravity = 9.81
+print(earthGravity)
+
+let lightSpeed: Int
+lightSpeed = 299792458
+print(lightSpeed)
+
+let appVersion: String
+appVersion = "1.2.3"
+print(appVersion)
+
+let developerName: String
+developerName = "감태"
+print(developerName)
+
+let taxRate: Float
+taxRate = 0.1
+print(taxRate)
+
+let currencySymbol: String
+currencySymbol = "$"
+print(currencySymbol)
+
+let daysInWeek: Int
+daysInWeek = 7
+print(daysInWeek)
+
+let monthsInYear: Int
+monthsInYear = 12
+print(monthsInYear)
+
+let maxScore: Int
+maxScore = 100
+print(maxScore)
+
+let minTemperature: Double
+minTemperature = -273.15
+print(minTemperature)
+
+let goldenRatio: Double
+goldenRatio = 1.618
+print(goldenRatio)
+
+let speedLimit: Int
+speedLimit = 120
+print(speedLimit)
+
+let universityName: String
+universityName = "서울대"
+print(universityName)
+
+let countryCode: String
+countryCode = "KR"
+print(countryCode)
+
+let defaultLanguage: String
+defaultLanguage = "Korean"
+print(defaultLanguage)
+
+let maxRetries: Int
+maxRetries = 3
+print(maxRetries)
+
+// 선언과 동시에 값을 할당하는 방법
+var fullName: String = "강춘장"
+print(fullName)
+
+var userAgeInYears: Int = 32
+print(userAgeInYears)
+
+var personHeight: Double = 180.3
+print(personHeight)
+
+var hasMembership: Bool = true
+print(hasMembership)
+
+var walletBalance: Float = 7890.50
+print(walletBalance)
+
+var homeTown: String = "서울"
+print(homeTown)
+
+var favoriteAnimal: String = "고양이"
+print(favoriteAnimal)
+
+var totalStepsToday: Int = 12000
+print(totalStepsToday)
+
+var currentTemperature: Double = 25.7
+print(currentTemperature)
+
+var isSnowing: Bool = false
+print(isSnowing)
+
+var examScore: Int = 87
+print(examScore)
+
+var leaderboardRank: Int = 4
+print(leaderboardRank)
+
+var hasPremiumAccess: Bool = false
+print(hasPremiumAccess)
+
+var seasonalDiscount: Double = 0.35
+print(seasonalDiscount)
+
+var bitcoinPrice: Float = 35000.75
+print(bitcoinPrice)
+
+var latestMessage: String = "반갑습니다."
+print(latestMessage)
+
+var pendingTasks: Int = 6
+print(pendingTasks)
+
+var lastLoginTime: String = "2025-03-12 09:30"
+print(lastLoginTime)
+
+var themePreference: String = "Light"
+print(themePreference)
+
+var phoneBatteryLevel: Int = 50
+print(phoneBatteryLevel)
+
+let mathConstantPi: Double = 3.141592
+print(mathConstantPi)
+
+let softwareName: String = "메주 씻기기"
+print(softwareName)
+
+let maxConnectionsAllowed: Int = 500000
+print(maxConnectionsAllowed)
+
+let minimumAgeRequirement: Int = 18
+print(minimumAgeRequirement)
+
+let gravityOnEarth: Double = 9.807
+print(gravityOnEarth)
+
+let maxSpeedOfLight: Int = 299792500
+print(maxSpeedOfLight)
+
+let applicationVersion: String = "5.4.1"
+print(applicationVersion)
+
+let creatorFullName: String = "강감태"
+print(creatorFullName)
+
+let standardTaxRate: Float = 0.07
+print(standardTaxRate)
+
+let defaultCurrencySign: String = "₩"
+print(defaultCurrencySign)
+
+let weekDayCount: Int = 7
+print(weekDayCount)
+
+let yearlyMonthCount: Int = 12
+print(yearlyMonthCount)
+
+let bestPossibleScore: Int = 150
+print(bestPossibleScore)
+
+let coldestPossibleTemp: Double = -273.16
+print(coldestPossibleTemp)
+
+let fibonacciGoldenRatio: Double = 1.618034
+print(fibonacciGoldenRatio)
+
+let roadSpeedLimit: Int = 130
+print(roadSpeedLimit)
+
+let famousUniversity: String = "서울대학교"
+print(famousUniversity)
+
+let internationalCountryCode: String = "한국"
+print(internationalCountryCode)
+
+let primaryLanguage: String = "한국어"
+print(primaryLanguage)
+
+let maxLoginAttempts: Int = 5
+print(maxLoginAttempts)
+
+// 타입을 사용하지 않고 바로 값을 할당하기
+var nickname = "춘심이"
+print(nickname)
+
+var age = 27
+print(age)
+
+var height = 174.2
+print(height)
+
+var isMember = false
+print(isMember)
+
+var balance = 1250.75
+print(balance)
+
+var city = "서울"
+print(city)
+
+var favoriteFood = "북어"
+print(favoriteFood)
+
+var dailyWorkoutTime = 45
+print(dailyWorkoutTime)
+
+var temperatureToday = 22.5
+print(temperatureToday)
+
+var isWindy = true
+print(isWindy)
+
+var gameLevel = 15
+print(gameLevel)
+
+var userScore = 89
+print(userScore)
+
+var isSubscriber = true
+print(isSubscriber)
+
+var discount = 0.18
+print(discount)
+
+var goldPrice = 68250.99
+print(goldPrice)
+
+var notification = "야옹!"
+print(notification)
+
+var pendingEmails = 3
+print(pendingEmails)
+
+var lastUpdate = "2025-03-12 18:45"
+print(lastUpdate)
+
+var appThemeSetting = "Dark"
+print(appThemeSetting)
+
+var batteryStatus = 72
+print(batteryStatus)
+
+let piValue = 3.141592
+print(piValue)
+
+let appTitle = "메주 괴롭히기"
+print(appTitle)
+
+let maxLoginCount = 10
+print(maxLoginCount)
+
+let minAge = 21
+print(minAge)
+
+let earthMass = 5.972e24
+print(earthMass)
+
+let lightSpeedKmS = 299792
+print(lightSpeedKmS)
+
+let appVersionNumber = "6.2.0"
+print(appVersionNumber)
+
+let founderName = "감튀"
+print(founderName)
+
+let salesTax = 0.12
+print(salesTax)
+
+let currencyType = "￥"
+print(currencyType)
+
+let daysPerWeek = 7
+print(daysPerWeek)
+
+let monthsPerYear = 12
+print(monthsPerYear)
+
+let highestPossibleGrade = 100
+print(highestPossibleGrade)
+
+let lowestRecordedTemp = -89.2
+print(lowestRecordedTemp)
+
+let phi = 1.618033
+print(phi)
+
+let maxCarSpeed = 250
+print(maxCarSpeed)
+
+let famousCollege = "설대"
+print(famousCollege)
+
+let countryCodeISO = "KR"
+print(countryCodeISO)
+
+let systemDefaultLanguage = "KO"
+print(systemDefaultLanguage)
+
+let maxRetriesAllowed = 4
+print(maxRetriesAllowed)
+
+// 편한 방법으로 본인의 이름, 취미 변수를 만들어보기
+var myName = "지성"
+var myHobby = "코딩"
+print("\(myName)입니다. 제 취미는 \(myHobby)입니다.")
