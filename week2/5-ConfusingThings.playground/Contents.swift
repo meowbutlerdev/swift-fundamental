e2f57e1547e52bfa9ce65d357bf85b65c8e6ab25
JISUNG PARK <meowbutlerdev@gmail.com>
2025-03-11 15:26:19 +0900

    docs: 헷갈리는 것들

---
+++ week2/5-ConfusingThings.playground/Contents.swift
@@ -0,0 +1,14 @@
+/**
+ class와 struct
+ - init 명시 차이
+ - 인스턴스 출력의 차이
+ - 인스턴스 타입 (참조, 값)
+
+ method와 fucntion
+ - method는 class, struct, enum 내부에 정의된 함수
+ - function은 class, struct, enum 외부에 정의된 함수
+
+ property와 변수/상수
+ - property는 class, struct, enum 내부에 정의된 변수/상수
+ - 변수/상수는 class, struct, enum 외부에 정의됨
+ */
