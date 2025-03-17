f22991675d4b0da5c76236f8c73298e2fd118ad3
JISUNG PARK <meowbutlerdev@gmail.com>
2025-03-17 14:17:54 +0900

    docs: 데이터 타입 저장 방식 정리

---
+++ week4/2-Data-Type.playground/Contents.swift
@@ -0,0 +1,13 @@
+/**
+ Value Type
+ - 기본 데이터 타입
+   - Int, String, Float, Double, Bool 등
+ - Collection 데이터 타입
+   - Array, Dictionary, Set 등
+ - enum
+ - struct
+
+ Reference Type
+ - class
+ - 클로저
+ */
